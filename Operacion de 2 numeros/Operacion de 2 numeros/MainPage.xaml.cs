0ac6f8a3633d664f6db5dd646777a10a37023340
Santiago <ds21tm.moyano.santiago@gmail.com>
2023-10-09 00:36:34 -0300

    Agregar archivos de proyecto.

---
+++ Operacion de 2 numeros/Operacion de 2 numeros/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Operacion_de_2_numeros
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var dato1 = double.Parse(Dato1.Text);
+            var dato2 = double.Parse(Dato2.Text);
+            var resultado = dato1 + dato2;
+            Resultado.Text = resultado.ToString();
+            string mensaje = "";
+        }
+    }
+}
